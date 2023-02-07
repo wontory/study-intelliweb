9763ca299f7b701b82081cf857202b98ccc44fa9
Wontory <tjddnjsjo@kakao.com>
2023-02-08 04:00:24 +0900

    props, 컴포넌트 분리, Wrapper Component

--- React/react-complete-guide/src/components/ExpenseItem.js
+++ React/react-complete-guide/src/components/ExpenseItem.js
@@ -1,18 +1,16 @@
+import ExpenseDate from "./ExpenseDate";
+import Card from "./Card";
 import "./ExpenseItem.css";
 
-const ExpenseItem = () => {
-  const expenseDate = new Date(2021, 3, 28);
-  const expenseTitle = 'Car Insurance';
-  const expenseAmount = 294.67;
-
+const ExpenseItem = (props) => {
   return (
-    <div className="expense-item">
-      <div>{expenseDate.toISOString()}</div>
+    <Card className="expense-item">
+      <ExpenseDate date={props.date} />
       <div className="expense-item__description">
-        <h2>{expenseTitle}</h2>
-        <div className="expense-item__price">${expenseAmount}</div>
+        <h2>{props.title}</h2>
+        <div className="expense-item__price">${props.amount}</div>
       </div>
-    </div>
+    </Card>
   );
 };
 

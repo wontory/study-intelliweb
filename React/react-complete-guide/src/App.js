359fdeb3274b463386b475866806cb7026d1afbd
Wontory <tjddnjsjo@kakao.com>
2023-02-09 23:39:19 +0900

    state handling & bottom-up component communication

--- React/react-complete-guide/src/App.js
+++ React/react-complete-guide/src/App.js
@@ -1,3 +1,6 @@
+import React from "react";
+
+import NewExpense from "./components/NewExpense/NewExpense";
 import Expenses from "./components/Expenses/Expenses";
 
 function App() {
@@ -23,9 +26,14 @@ function App() {
     },
   ];
 
+  const addExpenseHandler = (expense) => {
+    console.log("In App.js");
+    console.log(expense);
+  };
+
   return (
     <div>
-      <h2>Let's get started!</h2>
+      <NewExpense onAddExpense={addExpenseHandler} />
       <Expenses items={expenses} />
     </div>
   );

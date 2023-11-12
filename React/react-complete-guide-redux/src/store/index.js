9d667838f45f0ce67bc21f018fe2b3e9957aa638
조성원 <tjddnjsjo@kakao.com>
2023-11-12 23:42:17 +0900

    feat: State slice 추가하기

--- React/react-complete-guide-redux/src/store/index.js
+++ React/react-complete-guide-redux/src/store/index.js
@@ -1,6 +1,26 @@
 import { createStore } from 'redux';
+import { createSlice } from '@reduxjs/toolkit';
 
 const initialState = { counter: 0, showCounter: true };
+
+createSlice({
+  name: 'counter',
+  initialState,
+  reducers: {
+    increment(state) {
+      state.counter++; // immer 라이브러리를 사용하여 state를 "복제"한다.
+    },
+    decrement(state) {
+      state.counter--;
+    },
+    increase(state, action) {
+      state.counter = state.counter + action.amount;
+    },
+    toggleCounter(state) {
+      state.showCounter = !state.showCounter;
+    },
+  },
+});
 
 const counterReducer = (state = initialState, action) => {
   if (action.type === 'increment') {
